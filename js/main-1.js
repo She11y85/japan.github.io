e6cb7c785ee763696c3e7f256573c36baa08f3d7
ひろみ <a0931730082@gmail.com>
2024-06-20 13:47:23 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   HistoricBuildings.css
    	modified:   HistoricBuildings.html
    	modified:   js/main-1.js
    	modified:   style.css

--- js/main-1.js
+++ js/main-1.js
@@ -55,6 +55,8 @@ $(document).ready(function () {
                 newItems = itemsSmall;
             } else if (windowWidth <= 820) {
                 newItems = itemsMedium;
+            } else if (windowWidth <= 1440) { // 新增這個判斷條件
+                newItems = 4; // 設置為3個要顯示的項目
             } else {
                 newItems = itemsDefault;
             }
@@ -73,6 +75,47 @@ $(document).ready(function () {
     }
 
     initializeOwl('.slider', 5, 3, 1);
+    initializeOwl('.slider2', 3, 1, 1);
+});
+
+
+$(document).ready(function () {
+    function initializeOwl(selector, itemsDefault, itemsMedium, itemsSmall) {
+        var owl = $(selector).owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: selector === '.slider2' ? 3000 : 4500,
+            autoplayHoverPause: true,
+            items: itemsDefault
+        });
+
+        function updateOwlItems() {
+            var windowWidth = $(window).width();
+            var newItems;
+            if (windowWidth <= 640) {
+                newItems = itemsSmall;
+            } else if (windowWidth <= 820) {
+                newItems = itemsMedium;
+            } else if (windowWidth <= 1440) { // 新增這個判斷條件
+                newItems = 2; // 設置為3個要顯示的項目
+            } else {
+                newItems = itemsDefault;
+            }
+            owl.trigger('destroy.owl.carousel');
+            owl.owlCarousel({
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: selector === '.slider2' ? 3000 : 4500,
+                autoplayHoverPause: true,
+                items: newItems
+            });
+        }
+
+        $(window).resize(updateOwlItems);
+        updateOwlItems(); // Initialize on page load
+    }
+
+  2
     initializeOwl('.slider2', 3, 1, 1);
 });
 
